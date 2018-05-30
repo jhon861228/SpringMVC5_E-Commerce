de663c31fa86e5e76651724abfd9e5f128d3c746
Dimou John <33698076+Dimou-John@users.noreply.github.com>
2018-05-30 20:13:58 +0300

    Create Language.cs

---
+++ Language.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ForceJavaGitHub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Java repository");
+        }
+    }
+}
